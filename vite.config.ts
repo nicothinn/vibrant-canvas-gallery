099357c22c47647cab09571e5538e8d96e54f5c5
nicothinn <nicolas.pena_irurita@uao.edu.co>
2025-06-01 00:05:49 -0500

    Primera versión

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/vibrant-canvas-gallery/", // ⚠️ nombre exacto del repositorio de GitHub
   server: {
     host: "::",
     port: 8080,
